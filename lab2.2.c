0c02b2d2215b64653a0c669170f63a0b8f808f70
cmertenok <artempozitiv56@gmail.com>
2020-12-22 00:38:30 +0200

    Update lab2.2.c

--- lab2.2.c
+++ lab2.2.c
@@ -1,32 +1,60 @@
-﻿
-#include <stdio.h>
-#include <math.h>
+﻿#include <stdio.h>
 
-int main() {
+int main()
+{
+	int A[9][7] = {
+	{53,21,43,73,97,75,63},          
+	{51,34,25,83,33,85,14},
+	{45,42,39,64,91,64,24},
+	{46,26,10,44,92,70,82},
+	{52,52,28,59,95,19,93},
+	{25,60,56,41,76,34,58},
+	{73,89,88,29,48,92,97},
+	{84,79,77,38,87,54,64}
+	};
 
-	int n;
-	double S = 0;
-	double up = 0;
-	double down = 1;
-	int counter = 0;
-	printf("Input your n: ");
-	scanf_s("%d", &n);
-
-	
-	for (int i = 1; i <= n; i++) {
-	
-	    up = 3 - sin(i) * sin(i);
-		counter += 4;
-		down *= (log(i + 2));
-		counter += 3;
-		S += (up / down);
-		counter += 2;
+	printf("The initial matrix is:\n\n");
+	for (int f = 0; f < 8; f++)
+	{
+		for (int g = 0; g < 7; g++)
+			printf(" %d", A[f][g]);
+		printf("\n");
 	}
-	
-	
-	printf("The amount of operations: %d \n", counter);
-	printf("Result: %.7f", S);
+	printf("\n");
 
-	return 0;
-    
+	int i, j, L, R;
+	int n = 9;
+	float T;
+
+	for (int column = 0; column <= 6; column++) {
+		for (i = 1; i < n - 1; i++) {
+			T = A[i][column];
+			L = 0;
+			R = i;
+
+			while (L < R) {
+				j = (L + R) / 2;
+
+				if (A[j][column] <= T) {
+					L = j + 1;
+				}
+				else {
+					R = j;
+				}
+			}
+			for (int k = i - 1; k >= R; k--) {
+
+				A[k + 1][column] = A[k][column];
+			}
+			A[R][column] = T;
+		}
+	}
+	printf("The sorted matrix is:\n\n");
+	for (int f = 0; f < 8; f++)
+	{
+		for (int g = 0; g < 7; g++)
+
+			printf(" %d", A[f][g]);
+		printf("\n");
+	}
 }
