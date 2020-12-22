2654db6cb9034373665062a0ec4026e2f77b8f00
cmertenok <artempozitiv56@gmail.com>
2020-12-23 01:46:44 +0200

    Create KR.c

---
+++ KR.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+#define A 5
+
+int main(){
+    double X[A];
+    double sum;
+    double x = 4.337;
+    X[0] = x;
+    for (int i = 1; i < A; i++) {
+        sum = 0;
+        for (int k = 1; k <= i; k++) {
+            sum += (pow(-1, k + 1)) * (pow(x - 1, k) / k) * (pow(x, k));
+            X[i] = sum;
+        }
+    }
+    printf("Result: %f", sum);
+    return 0;
+}
